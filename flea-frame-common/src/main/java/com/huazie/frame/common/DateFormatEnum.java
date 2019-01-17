44ea346b5de2254b71068859e1e5ae9afdfbe15a
huazie <huazie.lgh@gmail.com>
2019-01-18 00:11:41 +0800

    huazie_flea-frame-common模块日期处理改造_MOD

--- flea-frame-common/src/main/java/com/huazie/frame/common/DateFormatEnum.java
+++ flea-frame-common/src/main/java/com/huazie/frame/common/DateFormatEnum.java
@@ -1,43 +1,40 @@
 package com.huazie.frame.common;
 
 /**
- * <p>
- * 		日期格式化枚举
- * </p>
- *  
+ * <p>日期格式化类型枚举</p>
+ *
  * @author huazie
  * @version v1.0.0
- * @date 2018年1月24日
- *
+ * @since 1.0.0
  */
 public enum DateFormatEnum {
-	
-	YYYY("yyyy"),
-	YYYYMM("yyyyMM"),
-	YYMM("yyMM"),
-	YYYYMMDD("yyyyMMdd"),
-	YYMMDD("yyMMdd"),
-	YYYYMMDDHHMMSS("yyyyMMddHHmmss"),
-	YYMMDDHHMMSS("yyMMddHHmmss"),
-	HHMMSS("HHmmss"),
-	YYYY_MM("yyyy-MM"),
-	YYYY_MM_DD("yyyy-MM-dd"),
-	YYYY_MM_DDHH_MM_SS("yyyy-MM-dd HH:mm:ss"),
-	HH_MM_SS("HH:mm:ss");
-	
-	private String format;
 
-	DateFormatEnum(String format) {
-		this.format = format;
-	}
-	
-	public String getFormat() {
-		return format;
-	}
+    YYYY("yyyy"),
+    YYYYMM("yyyyMM"),
+    YYMM("yyMM"),
+    YYYYMMDD("yyyyMMdd"),
+    YYMMDD("yyMMdd"),
+    YYYYMMDDHHMMSS("yyyyMMddHHmmss"),
+    YYMMDDHHMMSS("yyMMddHHmmss"),
+    HHMMSS("HHmmss"),
+    YYYY_MM("yyyy-MM"),
+    YYYY_MM_DD("yyyy-MM-dd"),
+    YYYY_MM_DDHH_MM_SS("yyyy-MM-dd HH:mm:ss"),
+    HH_MM_SS("HH:mm:ss");
 
-	private static DateFormatEnum getDefaultEnum() {
-		return YYYY_MM_DDHH_MM_SS;
-	}
-	
-	public static String DATA_FORMAT_DEFAULT = getDefaultEnum().format;
+    private String format;
+
+    DateFormatEnum(String format) {
+        this.format = format;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public static DateFormatEnum getDefaultEnum() {
+        return YYYY_MM_DDHH_MM_SS;
+    }
+
+    public static String DATE_FORMAT_DEFAULT = getDefaultEnum().format;
 }
--- flea-frame-common/src/test/java/com/huazie/frame/common/util/DateUtilsTest.java
+++ flea-frame-common/src/test/java/com/huazie/frame/common/util/DateUtilsTest.java
@@ -17,7 +17,17 @@ public class DateUtilsTest {
 	private static final Logger LOGGER = LoggerFactory.getLogger(DateUtilsTest.class);
 	
 	@Test
-	public void testDate(){
-		LOGGER.debug(DateUtils.date2String(null, DateFormatEnum.YYYY_MM_DDHH_MM_SS));
+	public void testDate2String(){
+		LOGGER.debug(DateUtils.date2String(null, DateFormatEnum.YYMMDDHHMMSS.getFormat()));
+		LOGGER.debug(DateUtils.date2String(null, DateFormatEnum.YYYY_MM_DD));
+		LOGGER.debug(DateUtils.date2String(null));
+	}
+
+	@Test
+	public void testString2Date(){
+		String dateStr = DateUtils.date2String(null);
+		LOGGER.debug("DATE1: {}", DateUtils.string2Date(dateStr, DateFormatEnum.YYYY_MM_DD.getFormat()));
+		LOGGER.debug("DATE2: {}", DateUtils.string2Date(dateStr, DateFormatEnum.YYYY_MM));
+		LOGGER.debug("DATE3: {}", DateUtils.string2Date(dateStr));
 	}
 }
