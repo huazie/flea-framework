82c16a5ca485310bf4990c81e07154999a651cb5
huazie <huazie.lgh@gmail.com>
2019-05-06 18:54:08 +0800

    huazie_flea-frame-tools新增工具类模块_MOD

---
+++ flea-frame-tools/src/main/java/com/huazie/frame/tools/i18n/FleaI18NMain.java
@@ -0,0 +1,451 @@
+package com.huazie.frame.tools.i18n;
+
+import com.huazie.frame.common.CommonConstants;
+import com.huazie.frame.common.FleaFrameManager;
+import com.huazie.frame.common.i18n.FleaI18nHelper;
+import com.huazie.frame.common.util.StringUtils;
+import com.huazie.frame.common.util.UnicodeUtils;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+
+/**
+ *  I18N 图形化转化类，将Unicode字符和本地环境相互转化
+ *  
+ * @author huazie
+ * @version v1.0.0
+ * @date 2018年8月20日
+ *
+ */
+@SuppressWarnings("serial")
+public class FleaI18NMain extends JFrame{
+	
+	private JTabbedPane mTabbedPane = new JTabbedPane(SwingConstants.TOP);// 选项卡
+	
+	private JPanel nativeToUnicodeFilePanel;	// 文件本地转Unicode
+	private JPanel unicodeToNativeFilePanel;	// 文件Unicode转本地
+	private JPanel nativeToUnicodeStringPanel;	// 字符本地转Unicode
+	private JPanel unicodeToNativeStringPanel;	// 字符Unicode转本地
+	private JPanel operatePanel;				// 操作按钮面板
+	
+	private JTextField nativeFileText;			// 本地文件输入框
+	private JButton nativeFileSelectBtn;		// 本地文件选择按钮
+	private JTextArea unicodeFilePathArea;		// Unicode生成文件路径
+	
+	private JTextField unicodeFileText; 		// Unicode文件输入框
+	private JButton unicodeFileSelectBtn;		// Unicode文件选择按钮
+	private JTextArea nativeFilePathArea;		// 本地生成文件路径
+	
+	private JTextField nativeStringText; 		// 本地字符串输入框
+	private JTextArea unicodeStringArea;		// Unicode文本域
+	
+	private JTextField unicodeStringText;		// Unicode字符串输入框
+	private JTextArea nativeStringArea;			// 本地文本域
+	
+	private JFileChooser fileChooser; 			// 文件选择
+	
+	private JButton open;		// 打开生成文件目录
+	private JButton convert;	// 确定按钮
+	private JButton reset;		// 重置按钮
+	
+	private int currTab = 1;					// 当前TAB页
+	
+	private static String EXE_PATH_DEFAULT = "F:\\FileRecv\\ubuntu\\Java\\bin\\Unicode\\jdk1.7.0_17\\bin\\native2ascii.exe";
+	
+	public FleaI18NMain() throws Exception{
+		super(FleaI18nHelper.i18nForCommon("COMMON_I18N_00000"));
+		init();
+	}
+	
+	public void init() throws Exception{
+		
+		setBounds(200, 20, (int) (400 / 0.618), 400);
+		int w = (Toolkit.getDefaultToolkit().getScreenSize().width - getWidth()) / 2;
+		int h = (Toolkit.getDefaultToolkit().getScreenSize().height - getHeight()) / 2;
+		setLocation(w, h);
+		setLayout(new GridLayout(2, 1));
+		
+		initFileNativeToUnicode(); 	// 初始化本地文件转Unicode文件页面
+		
+		initFileUnicodeToNative(); 	// 初始化Unicode文件转本地文件页面
+		
+		initStringNativeToUnicode();// 初始化本地字符串转Unicode字符串页面
+		
+		initStringUnicodeToNative();// 初始化Unicode字符串转本地字符串页面
+		
+		mTabbedPane.add(nativeToUnicodeFilePanel, FleaI18nHelper.i18nForCommon("COMMON_I18N_00001"));
+		mTabbedPane.add(unicodeToNativeFilePanel, FleaI18nHelper.i18nForCommon("COMMON_I18N_00002"));
+		mTabbedPane.add(nativeToUnicodeStringPanel, FleaI18nHelper.i18nForCommon("COMMON_I18N_00003"));
+		mTabbedPane.add(unicodeToNativeStringPanel, FleaI18nHelper.i18nForCommon("COMMON_I18N_00004"));
+		
+		mTabbedPane.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent paramChangeEvent) {
+				int selectedIndex = mTabbedPane.getSelectedIndex();
+				currTab = selectedIndex + 1;
+				if(currTab == 1 || currTab == 2){
+					open.setVisible(true);
+				}else if (currTab == 3 || currTab == 4){
+					open.setVisible(false);
+				}
+			}
+		});
+		
+		add(mTabbedPane);
+		
+		operatePanel = new JPanel();
+		
+		open = new JButton(FleaI18nHelper.i18nForCommon("COMMON_I18N_00005"));
+		open.setPreferredSize(new Dimension(80, 30));
+		open.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					open();
+				} catch (Exception e1) {
+				}
+			}
+		});
+		operatePanel.add(open);
+		
+		convert = new JButton(FleaI18nHelper.i18nForCommon("COMMON_I18N_00006"));
+		convert.setPreferredSize(new Dimension(80, 30));
+		convert.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if(currTab == 1){
+						fileNativeToUnicode();// 本地文件转Unicode文件
+					}else if (currTab == 2){
+						fileUnicodeToNative();// Unicode文件转本地文件
+					}else if(currTab == 3){
+						stringNativeToUnicode();// 本地字符串转Unicode字符串
+					}else if (currTab == 4){
+						stringUnicodeToNative();// Unicode字符串转本地字符串
+					}
+				} catch (Exception e1) {
+				}
+			}
+		});
+		operatePanel.add(convert);
+		
+		reset = new JButton(FleaI18nHelper.i18nForCommon("COMMON_I18N_00007"));
+		reset.setPreferredSize(new Dimension(80, 30));
+		reset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(currTab == 1){
+					nativeFileText.setText("");
+				}else if(currTab == 2){
+					unicodeFileText.setText("");
+				}else if(currTab == 3){
+					nativeStringText.setText("");
+					unicodeStringArea.setText("");
+				}else if(currTab == 4){
+					unicodeStringText.setText("");
+					nativeStringArea.setText("");
+				}
+			}
+		});
+		operatePanel.add(reset);
+		add(operatePanel);
+		
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+	}
+	
+	/**
+	 * 初始化本地文件转Unicode文件页面
+	 * 
+	 * @date 2018年10月29日
+	 *
+	 */
+	private void initFileNativeToUnicode()throws Exception{
+		JLabel nativeFileLabel = new JLabel(FleaI18nHelper.i18nForCommon("COMMON_I18N_00001"), JLabel.CENTER);
+		nativeFileLabel.setFont(new Font("宋体", Font.PLAIN, 16)); // 设置"本地文件"字体
+		nativeFileLabel.setPreferredSize(new Dimension(100, 30));
+		nativeFileText = new JTextField(30);// 本地文件路径输入框
+		nativeFileText.setPreferredSize(new Dimension(250, 30));
+		nativeFileText.setSelectedTextColor(Color.RED);
+		nativeFileSelectBtn = new JButton(FleaI18nHelper.i18nForCommon("COMMON_I18N_00008"));// 本地文件选择按钮
+		nativeFileSelectBtn.setPreferredSize(new Dimension(80, 30));
+		nativeFileSelectBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					selectFile();
+				} catch (Exception ex) {
+				}
+			}
+		});
+		unicodeFilePathArea = new JTextArea(6, 50);// Unicode文本域
+		unicodeFilePathArea.setSelectedTextColor(Color.RED);// 设置选中文本的颜色
+		unicodeFilePathArea.setLineWrap(true);// 设置自动换行
+		unicodeFilePathArea.setWrapStyleWord(true);// 设置换行不断字
+		unicodeFilePathArea.setEditable(false);
+		
+		// File Native To Unicode
+		nativeToUnicodeFilePanel = new JPanel();
+		nativeToUnicodeFilePanel.add(nativeFileLabel);
+		nativeToUnicodeFilePanel.add(nativeFileText);
+		nativeToUnicodeFilePanel.add(nativeFileSelectBtn);
+		nativeToUnicodeFilePanel.add(unicodeFilePathArea);
+	}
+	
+	/**
+	 * 初始化Unicode文件转本地文件页面
+	 * 
+	 * @date 2018年10月29日
+	 *
+	 */
+	private void initFileUnicodeToNative()throws Exception{
+		JLabel unicodeFileLabel = new JLabel(FleaI18nHelper.i18nForCommon("COMMON_I18N_00002"), JLabel.CENTER);
+		unicodeFileLabel.setFont(new Font("宋体", Font.PLAIN, 16)); // 设置"Unicode文件"字体
+		unicodeFileLabel.setPreferredSize(new Dimension(100, 30));
+		unicodeFileText = new JTextField(30);// Unicode文件路径输入框
+		unicodeFileText.setPreferredSize(new Dimension(240, 30));
+		unicodeFileText.setSelectedTextColor(Color.RED);
+		unicodeFileSelectBtn = new JButton(FleaI18nHelper.i18nForCommon("COMMON_I18N_00008"));// Unicode文件选择按钮
+		unicodeFileSelectBtn.setPreferredSize(new Dimension(80, 30));
+		unicodeFileSelectBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					selectFile();
+				} catch (Exception ex) {
+				}
+			}
+		});
+		nativeFilePathArea = new JTextArea(6, 50);// Unicode文本域
+		nativeFilePathArea.setSelectedTextColor(Color.RED);// 设置选中文本的颜色
+		nativeFilePathArea.setLineWrap(true);// 设置自动换行
+		nativeFilePathArea.setWrapStyleWord(true);// 设置换行不断字
+		nativeFilePathArea.setEditable(false);
+		
+		// File Unicode To Native
+		unicodeToNativeFilePanel = new JPanel();
+		unicodeToNativeFilePanel.add(unicodeFileLabel);
+		unicodeToNativeFilePanel.add(unicodeFileText);
+		unicodeToNativeFilePanel.add(unicodeFileSelectBtn);
+		unicodeToNativeFilePanel.add(nativeFilePathArea);
+	}
+	
+	/**
+	 * 初始化本地字符串转Unicode字符串页面
+	 * 
+	 * @date 2018年10月29日
+	 *
+	 */
+	private void initStringNativeToUnicode()throws Exception{
+		JLabel nativeStringLabel = new JLabel(FleaI18nHelper.i18nForCommon("COMMON_I18N_00003"), JLabel.CENTER);
+		nativeStringLabel.setFont(new Font("宋体", Font.PLAIN, 18)); // 设置"本地文件"字体
+		nativeStringLabel.setPreferredSize(new Dimension(120, 30));
+		nativeStringText = new JTextField(36);// 本地文件路径输入框
+		nativeStringText.setPreferredSize(new Dimension(350, 30));
+		nativeStringText.setSelectedTextColor(Color.RED);
+		unicodeStringArea = new JTextArea(6, 50);// Unicode文本域
+		unicodeStringArea.setSelectedTextColor(Color.RED);// 设置选中文本的颜色
+		unicodeStringArea.setLineWrap(true);// 设置自动换行
+		unicodeStringArea.setWrapStyleWord(true);// 设置换行不断字
+		
+		// String Native To Unicode
+		nativeToUnicodeStringPanel = new JPanel();
+		nativeToUnicodeStringPanel.add(nativeStringLabel);
+		nativeToUnicodeStringPanel.add(nativeStringText);
+		nativeToUnicodeStringPanel.add(unicodeStringArea);
+	}
+	
+	/**
+	 * 初始化Unicode字符串转本地字符串页面
+	 * 
+	 * @date 2018年10月29日
+	 *
+	 */
+	private void initStringUnicodeToNative()throws Exception{
+		JLabel unicodeStringLabel = new JLabel(FleaI18nHelper.i18nForCommon("COMMON_I18N_00004"), JLabel.CENTER);
+		unicodeStringLabel.setFont(new Font("宋体", Font.PLAIN, 18)); // 设置"Unicode文件"字体
+		unicodeStringLabel.setPreferredSize(new Dimension(130, 30));
+		unicodeStringText = new JTextField(35);// Unicode文件路径输入框
+		unicodeStringText.setPreferredSize(new Dimension(350, 30));
+		unicodeStringText.setSelectedTextColor(Color.RED);
+		nativeStringArea = new JTextArea(6, 50);// 本地文本域
+		nativeStringArea.setSelectedTextColor(Color.RED);// 设置选中文本的颜色
+		nativeStringArea.setLineWrap(true);// 设置自动换行
+		nativeStringArea.setWrapStyleWord(true);// 设置换行不断字
+		
+		// String Unicode To Native
+		unicodeToNativeStringPanel = new JPanel();
+		unicodeToNativeStringPanel.add(unicodeStringLabel);
+		unicodeToNativeStringPanel.add(unicodeStringText);
+		unicodeToNativeStringPanel.add(nativeStringArea);
+	}
+	
+	/**
+	 * 打开文件目录
+	 * 
+	 * @date 2018年10月29日
+	 *
+	 */
+	private void open()throws Exception{
+		String path = null;
+		if(currTab == 1){
+			// 获取生成的文件路径
+			path = unicodeFilePathArea.getText();
+		}else if (currTab == 2){
+			// 获取生成的文件路径
+			path = nativeFilePathArea.getText();
+		}
+		// 打开 生成的文件 所在目录
+		if(StringUtils.isNotBlank(path)){
+			try {
+				Desktop.getDesktop().open(new File(path).getParentFile());
+			} catch (IOException e) {
+				showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00010"), "大佬，由于系统异常，打开生成文件 所在目录异常", JOptionPane.ERROR_MESSAGE);
+			}
+		}else{
+			if(currTab == 1){
+				showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00009"), FleaI18nHelper.i18nForCommon("COMMON_I18N_00015", new String[]{FleaI18nHelper.i18nForCommon("COMMON_I18N_00001")}), JOptionPane.WARNING_MESSAGE);
+			}else if(currTab == 2){
+				showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00009"), FleaI18nHelper.i18nForCommon("COMMON_I18N_00015", new String[]{FleaI18nHelper.i18nForCommon("COMMON_I18N_00002")}), JOptionPane.WARNING_MESSAGE);
+			}
+		}
+	}
+	
+	/**
+	 * 本地文件转Unicode文件
+	 * 
+	 * @date 2018年10月10日
+	 *
+	 */
+ 	private void fileNativeToUnicode()throws Exception{
+		// 获取本地文件的路径
+		String nativeFilePath = nativeFileText.getText();
+		if (StringUtils.isNotBlank(nativeFilePath)) {
+			// Unicode文件路径，默认取本地文件路径，文件名追加Unicode，文件后缀保持不变
+			String nativeFilePre = nativeFilePath.substring(0, nativeFilePath.lastIndexOf(CommonConstants.SymbolConstants.DOT));
+			String nativeFileExt = nativeFilePath.substring( nativeFilePath.lastIndexOf(CommonConstants.SymbolConstants.DOT), nativeFilePath.length());
+			String unicodeFilePath = nativeFilePre + CommonConstants.SymbolConstants.UNDERLINE + "Unicode" + nativeFileExt;
+			try {
+				UnicodeUtils.fileNativeToUnicode(EXE_PATH_DEFAULT, nativeFilePath, unicodeFilePath);
+				// 提示 转化成功，展示文件路径
+				showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00011"), FleaI18nHelper.i18nForCommon("COMMON_I18N_00016", new String[]{FleaI18nHelper.i18nForCommon("COMMON_I18N_00002"), unicodeFilePath}), JOptionPane.INFORMATION_MESSAGE);
+				unicodeFilePathArea.setText(unicodeFilePath);
+			} catch (Exception e) {
+				showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00010"), FleaI18nHelper.i18nForCommon("COMMON_I18N_00014"), JOptionPane.ERROR_MESSAGE);
+			}
+		} else {
+			showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00009"), FleaI18nHelper.i18nForCommon("COMMON_I18N_00012", new String[]{FleaI18nHelper.i18nForCommon("COMMON_I18N_00001")}), JOptionPane.WARNING_MESSAGE);
+		}
+	}
+	
+	/**
+	 * Unicode文件转本地文件
+	 * 
+	 * @date 2018年10月10日
+	 *
+	 */
+	private void fileUnicodeToNative()throws Exception {
+		// 获取Unicode文件路径
+		String unicodeFilePath = unicodeFileText.getText();
+		if(StringUtils.isNotBlank(unicodeFilePath)) {
+			// Unicode文件路径，默认取本地文件路径，文件名追加Unicode，文件后缀保持不变
+			String unicodeFilePre = unicodeFilePath.substring(0, unicodeFilePath.lastIndexOf(CommonConstants.SymbolConstants.DOT));
+			String unicodeFileExt = unicodeFilePath.substring(unicodeFilePath.lastIndexOf(CommonConstants.SymbolConstants.DOT), unicodeFilePath.length());
+			String nativeFilePath = unicodeFilePre + CommonConstants.SymbolConstants.UNDERLINE + "Native" + unicodeFileExt;
+			try {
+				UnicodeUtils.fileUnicodeToNative(EXE_PATH_DEFAULT, unicodeFilePath, nativeFilePath);
+				// 提示 转化成功，展示文件路径
+				showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00011"), FleaI18nHelper.i18nForCommon("COMMON_I18N_00016", new String[]{FleaI18nHelper.i18nForCommon("COMMON_I18N_00001"), nativeFilePath}), JOptionPane.INFORMATION_MESSAGE);
+				nativeFilePathArea.setText(nativeFilePath);
+			} catch (Exception e) {
+				showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00010"), FleaI18nHelper.i18nForCommon("COMMON_I18N_00014"), JOptionPane.ERROR_MESSAGE);
+			}
+		} else {
+			showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00009"), FleaI18nHelper.i18nForCommon("COMMON_I18N_00012", new String[]{FleaI18nHelper.i18nForCommon("COMMON_I18N_00002")}), JOptionPane.WARNING_MESSAGE);
+		}
+	}
+	
+	/**
+	 * 本地字符串转Unicode字符串
+	 * 
+	 * @date 2018年10月10日
+	 *
+	 */
+	private void stringNativeToUnicode()throws Exception{
+		// 获取本地字符串
+		String nativeStr = nativeStringText.getText();
+		if(StringUtils.isNotBlank(nativeStr)) {
+			try {
+				String unicodeStr = UnicodeUtils.nativeToUnicode(nativeStr);
+				unicodeStringArea.setText(unicodeStr);
+			} catch (Exception e) {
+				showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00010"), FleaI18nHelper.i18nForCommon("COMMON_I18N_00014"), JOptionPane.ERROR_MESSAGE);
+			}
+		} else {
+			showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00009"), FleaI18nHelper.i18nForCommon("COMMON_I18N_00013", new String[]{FleaI18nHelper.i18nForCommon("COMMON_I18N_00003")}), JOptionPane.WARNING_MESSAGE);
+		}
+	}
+	
+	/**
+	 * Unicode字符串转本地字符串
+	 * 
+	 * @date 2018年10月10日
+	 *
+	 */
+	private void stringUnicodeToNative()throws Exception {
+		// 获取Unicode字符串
+		String unicodeStr = unicodeStringText.getText();
+		if(StringUtils.isNotBlank(unicodeStr)){
+			try {
+				String nativeStr = UnicodeUtils.unicodeToNative(unicodeStr);
+				nativeStringArea.setText(nativeStr);
+			} catch (Exception e) {
+				showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00010"), FleaI18nHelper.i18nForCommon("COMMON_I18N_00014"), JOptionPane.ERROR_MESSAGE);
+			}
+		}else {
+			showTips(FleaI18nHelper.i18nForCommon("COMMON_I18N_00009"), FleaI18nHelper.i18nForCommon("COMMON_I18N_00013", new String[]{FleaI18nHelper.i18nForCommon("COMMON_I18N_00004")}), JOptionPane.WARNING_MESSAGE);
+		}
+	}
+	
+	/**
+	 * 展示提示消息
+	 * 
+	 * @date 2018年10月10日
+	 *
+	 * @param title
+	 * @param msg
+	 * @param messageType
+	 */
+	private void showTips(String title, String msg, int messageType){
+		JOptionPane.showMessageDialog(null, msg, title, messageType);  
+	}
+	
+	/**
+	 * 弹出选择文件页面
+	 * 
+	 * @date 2018年10月29日
+	 *
+	 */
+	private void selectFile()throws Exception{
+		fileChooser = new JFileChooser();
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);  
+		fileChooser.showDialog(new JLabel(), FleaI18nHelper.i18nForCommon("COMMON_I18N_00008"));
+		File file = fileChooser.getSelectedFile();
+		if(file != null){
+			if(currTab == 1){
+				nativeFileText.setText(file.getAbsolutePath());
+			}else if (currTab == 2){
+				unicodeFileText.setText(file.getAbsolutePath());
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		FleaFrameManager.getManager().setLocale(Locale.US);
+		FleaI18NMain main = new FleaI18NMain();
+		main.setVisible(true);
+	}
+
+}
+
