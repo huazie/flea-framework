b02e549c5f1663770a324443c8aa1c726586c0e8
huazie <huazie.lgh@gmail.com>
2019-09-03 00:07:59 +0800

    huazie_flea-frame-tools测试自动生成代码_MOD

--- flea-frame-tools/src/main/java/com/huazie/frame/tools/code/FleaCodeMain.java
+++ flea-frame-tools/src/main/java/com/huazie/frame/tools/code/FleaCodeMain.java
@@ -18,12 +18,12 @@ public class FleaCodeMain extends JFrame {
 
     private static String author = "huazie";
     private static String version = "1.0.0";
-    //private static String rootPackage = "E:\\fleaworkspace\\flea-frame\\flea-frame-auth\\src\\main\\java";
-    private static String rootPackage = "F:\\FileRecv\\project\\flea-frame\\flea-frame-auth\\src\\main\\java";
+    private static String rootPackage = "E:\\fleaworkspace\\flea-frame\\flea-frame-auth\\src\\main\\java";
+    //private static String rootPackage = "F:\\FileRecv\\project\\flea-frame\\flea-frame-auth\\src\\main\\java";
 
     private static String codePackage = "com.huazie.frame.auth.base.user";
     private static String fleaPersistenceUnitDaoClassPackage = "com.huazie.frame.auth.base";
-//    private static String fleaPersistenceUnitDaoClassName = "FleaAuthDAOImpl";
+    private static String fleaPersistenceUnitDaoClassName = "FleaAuthDAOImpl";
     private static String fleaPersistenceUnitName = "fleaauth";
     private static String fleaPersistenceUnitAliasName = "FleaAuth";
     private static String tableName = "flea_account_attr";
@@ -42,6 +42,16 @@ public class FleaCodeMain extends JFrame {
     }
 
     public void code() {
+        // 编写代码
+        FleaCodeProgrammer.code(createParamMap());
+    }
+
+    public void clean() {
+        // 清理代码
+        FleaCodeProgrammer.clean(createParamMap());
+    }
+
+    private Map<String, Object> createParamMap() {
         Map<String, Object> param = new HashMap<String, Object>();
         param.put(ToolsConstants.CodeConstants.AUTHOR, author);
         param.put(ToolsConstants.CodeConstants.VERSION, version);
@@ -50,35 +60,17 @@ public class FleaCodeMain extends JFrame {
         param.put(ToolsConstants.CodeConstants.CODE_PACKAGE, codePackage);
         param.put(ToolsConstants.CodeConstants.FLEA_PERSISTENCE_UNIT_DAO_CLASS_PACKAGE, fleaPersistenceUnitDaoClassPackage);
         // 1. 现有 持久化单元DAO层实现类
-//        param.put(ToolsConstants.CodeConstants.FLEA_PERSISTENCE_UNIT_DAO_CLASS_NAME, fleaPersistenceUnitDaoClassName);
+        param.put(ToolsConstants.CodeConstants.FLEA_PERSISTENCE_UNIT_DAO_CLASS_NAME, fleaPersistenceUnitDaoClassName);
         // 2. 新增 持久化单元DAO层实现类
-        param.put(ToolsConstants.CodeConstants.FLEA_PERSISTENCE_UNIT_NAME, fleaPersistenceUnitName);
-        param.put(ToolsConstants.CodeConstants.FLEA_PERSISTENCE_UNIT_ALIAS_NAME, fleaPersistenceUnitAliasName);
+//        param.put(ToolsConstants.CodeConstants.FLEA_PERSISTENCE_UNIT_NAME, fleaPersistenceUnitName);
+//        param.put(ToolsConstants.CodeConstants.FLEA_PERSISTENCE_UNIT_ALIAS_NAME, fleaPersistenceUnitAliasName);
 
         param.put(ToolsConstants.CodeConstants.TABLE_NAME, tableName);
         param.put(ToolsConstants.CodeConstants.TABLE_DESC, tableDesc);
 
         param.put(ToolsConstants.CodeConstants.DB_SYSTEM_NAME, dbSystemName);
         param.put(ToolsConstants.CodeConstants.DB_NAME, dbName);
-
-        // 编写代码
-        FleaCodeProgrammer.code(param);
-    }
-
-    public void clean() {
-        Map<String, Object> param = new HashMap<String, Object>();
-        param.put(ToolsConstants.CodeConstants.ROOT_PACKAGE, rootPackage);
-        param.put(ToolsConstants.CodeConstants.CODE_PACKAGE, codePackage);
-        param.put(ToolsConstants.CodeConstants.FLEA_PERSISTENCE_UNIT_DAO_CLASS_PACKAGE, fleaPersistenceUnitDaoClassPackage);
-        // 1. 现有 持久化单元DAO层实现类
-//        param.put(ToolsConstants.CodeConstants.FLEA_PERSISTENCE_UNIT_DAO_CLASS_NAME, fleaPersistenceUnitDaoClassName);
-        // 2. 新增 持久化单元DAO层实现类
-        param.put(ToolsConstants.CodeConstants.FLEA_PERSISTENCE_UNIT_NAME, fleaPersistenceUnitName);
-        param.put(ToolsConstants.CodeConstants.FLEA_PERSISTENCE_UNIT_ALIAS_NAME, fleaPersistenceUnitAliasName);
-        param.put(ToolsConstants.CodeConstants.TABLE_NAME, tableName);
-
-        // 清理代码
-        FleaCodeProgrammer.clean(param);
+        return param;
     }
 
 }
