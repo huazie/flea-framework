23347f4adb952d03ef61dcb4e8f5bb5ba8a8e2b8
huazie <huazie.lgh@gmail.com>
2019-11-06 15:34:47 +0800

    huazie_Flea Jersey 资源服务调用日志_ADD

---
+++ flea-frame-core/src/main/java/com/huazie/frame/core/base/cfgdata/dao/interfaces/IFleaJerseyResServiceLogDAO.java
@@ -0,0 +1,14 @@
+package com.huazie.frame.core.base.cfgdata.dao.interfaces;
+
+import com.huazie.frame.core.base.cfgdata.entity.FleaJerseyResServiceLog;
+import com.huazie.frame.db.jpa.dao.interfaces.IAbstractFleaJPADAO;
+
+/**
+ * <p> Flea Jersey资源服务调用日志DAO层接口 </p>
+ *
+ * @author huazie
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public interface IFleaJerseyResServiceLogDAO extends IAbstractFleaJPADAO<FleaJerseyResServiceLog> {
+}
---
+++ flea-frame-core/src/main/java/com/huazie/frame/core/base/cfgdata/service/impl/FleaJerseyResServiceLogSVImpl.java
@@ -0,0 +1,30 @@
+package com.huazie.frame.core.base.cfgdata.service.impl;
+
+import com.huazie.frame.core.base.cfgdata.dao.interfaces.IFleaJerseyResServiceLogDAO;
+import com.huazie.frame.core.base.cfgdata.entity.FleaJerseyResServiceLog;
+import com.huazie.frame.core.base.cfgdata.service.interfaces.IFleaJerseyResServiceLogSV;
+import com.huazie.frame.db.jpa.dao.interfaces.IAbstractFleaJPADAO;
+import com.huazie.frame.db.jpa.service.impl.AbstractFleaJPASVImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p> Flea Jersey资源服务调用日志SV层实现类 </p>
+ *
+ * @author huazie
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Service("fleaJerseyResServiceLogSV")
+public class FleaJerseyResServiceLogSVImpl extends AbstractFleaJPASVImpl<FleaJerseyResServiceLog> implements IFleaJerseyResServiceLogSV {
+
+    @Autowired
+    @Qualifier("fleaJerseyResServiceLogDAO")
+    private IFleaJerseyResServiceLogDAO fleaJerseyResServiceLogDao;
+
+    @Override
+    protected IAbstractFleaJPADAO<FleaJerseyResServiceLog> getDAO() {
+        return fleaJerseyResServiceLogDao;
+    }
+}
