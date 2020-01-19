558e0d6ce3b4309c2ed6d096fd30c993d4275a18
huazie <huazie.lgh@gmail.com>
2020-01-19 18:23:53 +0800

    huazie_父过滤器_MOD

---
+++ flea-frame-core/src/main/java/com/huazie/frame/core/filter/FleaFrameFilter.java
@@ -0,0 +1,34 @@
+package com.huazie.frame.core.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * <p> Flea Frame Filter </p>
+ *
+ * @author huazie
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class FleaFrameFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
