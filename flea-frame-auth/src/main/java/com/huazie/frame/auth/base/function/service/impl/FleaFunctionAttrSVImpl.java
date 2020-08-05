8a64321efdc0ee70ec209cd3f40d29b28421bf69
huazie <huazie.lgh@gmail.com>
2020-08-05 08:56:25 +0800

    huazie_授权模块之权限关联菜单，系统关联菜单查询_ADD

--- flea-frame-auth/src/main/java/com/huazie/frame/auth/base/function/service/impl/FleaFunctionAttrSVImpl.java
+++ flea-frame-auth/src/main/java/com/huazie/frame/auth/base/function/service/impl/FleaFunctionAttrSVImpl.java
@@ -3,11 +3,14 @@ package com.huazie.frame.auth.base.function.service.impl;
 import com.huazie.frame.auth.base.function.dao.interfaces.IFleaFunctionAttrDAO;
 import com.huazie.frame.auth.base.function.entity.FleaFunctionAttr;
 import com.huazie.frame.auth.base.function.service.interfaces.IFleaFunctionAttrSV;
+import com.huazie.frame.common.exception.CommonException;
 import com.huazie.frame.db.jpa.dao.interfaces.IAbstractFleaJPADAO;
 import com.huazie.frame.db.jpa.service.impl.AbstractFleaJPASVImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p> Flea功能扩展属性SV层实现类 </p>
@@ -25,6 +28,11 @@ public class FleaFunctionAttrSVImpl extends AbstractFleaJPASVImpl<FleaFunctionAt
     @Qualifier("fleaFunctionAttrDAO")
     public void setFleaFunctionAttrDao(IFleaFunctionAttrDAO fleaFunctionAttrDao) {
         this.fleaFunctionAttrDao = fleaFunctionAttrDao;
+    }
+
+    @Override
+    public List<FleaFunctionAttr> getFunctionAttrList(Long functionId, String functionType, String attrCode) throws CommonException {
+        return fleaFunctionAttrDao.getFunctionAttrList(functionId, functionType, attrCode);
     }
 
     @Override
