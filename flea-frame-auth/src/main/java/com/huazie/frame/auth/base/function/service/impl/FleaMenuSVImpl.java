dcd40adeb2f076d2d58698f266ee1d7867c2c7f8
huazie <huazie.lgh@gmail.com>
2020-08-13 14:36:28 +0800

    huazie_取用户授权的指定系统关联菜单_ADD

--- flea-frame-auth/src/main/java/com/huazie/frame/auth/base/function/service/impl/FleaMenuSVImpl.java
+++ flea-frame-auth/src/main/java/com/huazie/frame/auth/base/function/service/impl/FleaMenuSVImpl.java
@@ -3,11 +3,16 @@ package com.huazie.frame.auth.base.function.service.impl;
 import com.huazie.frame.auth.base.function.dao.interfaces.IFleaMenuDAO;
 import com.huazie.frame.auth.base.function.entity.FleaMenu;
 import com.huazie.frame.auth.base.function.service.interfaces.IFleaMenuSV;
+import com.huazie.frame.common.exception.CommonException;
+import com.huazie.frame.common.util.CollectionUtils;
 import com.huazie.frame.db.jpa.dao.interfaces.IAbstractFleaJPADAO;
 import com.huazie.frame.db.jpa.service.impl.AbstractFleaJPASVImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p> Flea菜单SV层实现类 </p>
@@ -25,6 +30,29 @@ public class FleaMenuSVImpl extends AbstractFleaJPASVImpl<FleaMenu> implements I
     @Qualifier("fleaMenuDAO")
     public void setFleaMenuDao(IFleaMenuDAO fleaMenuDao) {
         this.fleaMenuDao = fleaMenuDao;
+    }
+
+    @Override
+    public List<FleaMenu> getAllAccessibleMenus(List<Long> systemRelMenuIdList, List<Long> menuIdList) throws CommonException {
+
+        // 取交集
+        systemRelMenuIdList.retainAll(menuIdList);
+
+        // 获取菜单列表
+        List<FleaMenu> allAccessibleMenus = null;
+        if (CollectionUtils.isNotEmpty(systemRelMenuIdList)) {
+            allAccessibleMenus = new ArrayList<>();
+            for (Long menuId : systemRelMenuIdList) {
+                if (null != menuId && menuId > 0L) {
+                    FleaMenu fleaMenu = this.query(menuId);
+                    if (null != fleaMenu) {
+                        allAccessibleMenus.add(fleaMenu);
+                    }
+                }
+            }
+        }
+
+        return allAccessibleMenus;
     }
 
     @Override
--- flea-frame-auth/src/main/java/com/huazie/frame/auth/base/function/service/interfaces/IFleaMenuSV.java
+++ flea-frame-auth/src/main/java/com/huazie/frame/auth/base/function/service/interfaces/IFleaMenuSV.java
@@ -1,7 +1,10 @@
 package com.huazie.frame.auth.base.function.service.interfaces;
 
 import com.huazie.frame.auth.base.function.entity.FleaMenu;
+import com.huazie.frame.common.exception.CommonException;
 import com.huazie.frame.db.jpa.service.interfaces.IAbstractFleaJPASV;
+
+import java.util.List;
 
 /**
  * <p> Flea菜单SV层接口定义 </p>
@@ -11,4 +14,15 @@ import com.huazie.frame.db.jpa.service.interfaces.IAbstractFleaJPASV;
  * @since 1.0.0
  */
 public interface IFleaMenuSV extends IAbstractFleaJPASV<FleaMenu> {
+
+    /**
+     * <p> 获取用户授权的指定系统的菜单 </p>
+     *
+     * @param systemRelMenuIdList 系统关联菜单列表
+     * @param menuIdList          用户授权菜单列表
+     * @return 用于授权的指定系统的菜单
+     * @throws CommonException 通用异常
+     * @since 1.0.0
+     */
+    List<FleaMenu> getAllAccessibleMenus(List<Long> systemRelMenuIdList, List<Long> menuIdList) throws CommonException;
 }
