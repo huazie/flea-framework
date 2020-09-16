2805b8959a61f22fa48a9ec7e5d6bfef44cca62f
huazie <huazie.lgh@gmail.com>
2020-09-16 08:57:31 +0800

    huazie_简单优化_MOD

--- flea-frame-jersey/flea-frame-jersey-server/src/test/java/com/huazie/frame/jersey/server/cache/FleaAuthCacheTest.java
+++ flea-frame-jersey/flea-frame-jersey-server/src/test/java/com/huazie/frame/jersey/server/cache/FleaAuthCacheTest.java
@@ -25,7 +25,7 @@ public class FleaAuthCacheTest {
 
     @Test
     public void testGetFleaCache() {
-        Cache cache = CacheConfigManager.getCache("fleaauth");
+        Cache cache = CacheConfigManager.getCache("fleaauthmenu");
         LOGGER.debug("*****************************************Cache = {}", cache);
         CacheData cacheData = CacheConfigManager.getCacheData(cache.getType());
         LOGGER.debug("*****************************************CacheData = {}", cacheData);
