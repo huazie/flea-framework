b95aa530232fe33c530314a318edc2c56d90541f
huazie <huazie.lgh@gmail.com>
2021-06-05 00:11:14 +0800

    huazie_flea-frame-cache添加相关类备注_MOD

--- flea-frame-cache/src/main/java/com/huazie/frame/cache/redis/RedisFleaCacheBuilder.java
+++ flea-frame-cache/src/main/java/com/huazie/frame/cache/redis/RedisFleaCacheBuilder.java
@@ -14,10 +14,20 @@ import com.huazie.frame.common.util.CollectionUtils;
 import java.util.List;
 
 /**
- * <p> Redis的Flea缓存建造者实现 </p>
+ * Redis Flea缓存建造者实现类，用于整合各类缓存接入时创建Redis
+ * Flea缓存。
+ *
+ * <p>缓存定义文件【flea-cache.xml】中，每一个缓存定义配置都对应
+ * 一类缓存数据，每类缓存数据都归属一个缓存组，每个缓存组都映射着
+ * 具体的缓存实现名，而整合各类缓存接入时，每个具体的缓存实现名都
+ * 配置了Flea缓存建造着实现类。
+ *
+ * <p>可查看Flea缓存配置文件【flea-cache-config.xml】，
+ * 获取Redis Flea缓存建造者配置项【{@code <cache-item key="Redis">}】
  *
  * @author huazie
  * @version 1.0.0
+ * @see com.huazie.frame.cache.common.FleaCacheFactory
  * @since 1.0.0
  */
 public class RedisFleaCacheBuilder implements IFleaCacheBuilder {
--- flea-frame-cache/src/main/java/com/huazie/frame/cache/redis/RedisSpringCacheManager.java
+++ flea-frame-cache/src/main/java/com/huazie/frame/cache/redis/RedisSpringCacheManager.java
@@ -7,11 +7,11 @@ import com.huazie.frame.cache.redis.impl.RedisClientProxy;
 import com.huazie.frame.cache.redis.impl.RedisSpringCache;
 
 /**
- * Redis Spring缓存管理类, 用于接入Spring管理Redis缓存。
+ * Redis Spring缓存管理类, 用于接入Spring框架管理Redis缓存。
  *
  * <p> 它的默认构造方法，用于初始化Redis客户端, 这里需要先
  * 初始化Redis连接池，默认连接池名为default；然后通过Redis
- * 客户端代理类获取被代理的Redis客户端接口。
+ * 客户端代理类获取被代理的Redis客户端。
  *
  * <p> 方法 {@code newCache} 用于创建一个Redis Spring缓存，
  * 而它内部是由Redis Flea缓存实现具体的 读 {@code get}、
@@ -25,7 +25,7 @@ import com.huazie.frame.cache.redis.impl.RedisSpringCache;
  */
 public class RedisSpringCacheManager extends AbstractSpringCacheManager {
 
-    private RedisClient redisClient;
+    private RedisClient redisClient; // Redis客户端
 
     /**
      * <p> 默认构造方法，初始化Redis客户端 </p>
