c2ea18c6be3e67b3a9f1c629b639f9b671d8980c
Huazie <huazie.lgh@gmail.com>
2021-12-09 08:02:39 +0800

    huazie_flea-common本地国际化代码和相关配置优化_MOD

--- flea-common/src/main/java/com/huazie/fleaframework/common/i18n/FleaI18nResEnum.java
+++ flea-common/src/main/java/com/huazie/fleaframework/common/i18n/FleaI18nResEnum.java
@@ -10,11 +10,11 @@ package com.huazie.fleaframework.common.i18n;
 public enum FleaI18nResEnum {
 
     ERROR("error", "异常信息国际码资源文件类型"),
-    ERROR_CORE("error_core", "FLEA FRAME CORE异常信息国际码资源文件类型"),
-    ERROR_DB("error_db", "FLEA FRAME DB异常信息国际码资源文件类型"),
-    ERROR_JERSEY("error_jersey", "FLEA FRAME JERSEY异常信息国际码资源文件类型"),
-    ERROR_AUTH("error_auth", "FLEA FRAME AUTH异常信息国际码资源文件类型"),
-    AUTH("auth", "FLEA FRAME AUTH 国际码资源文件类型"),
+    ERROR_CORE("error_core", "FLEA CORE异常信息国际码资源文件类型"),
+    ERROR_DB("error_db", "FLEA DB异常信息国际码资源文件类型"),
+    ERROR_JERSEY("error_jersey", "FLEA JERSEY异常信息国际码资源文件类型"),
+    ERROR_AUTH("error_auth", "FLEA AUTH异常信息国际码资源文件类型"),
+    AUTH("auth", "FLEA AUTH 国际码资源文件类型"),
     COMMON("common", "公共信息国际码资源文件类型");
 
     private String resName;
