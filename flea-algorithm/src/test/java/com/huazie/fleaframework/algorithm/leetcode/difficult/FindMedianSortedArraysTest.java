ba4cdb09e2704dd2368af31d3dfcac629f42774a
Huazie <huazie.lgh@gmail.com>
2022-04-29 23:55:16 +0800

    新增leetcode的算法题【简单、中等、困难】

---
+++ flea-algorithm/src/test/java/com/huazie/fleaframework/algorithm/leetcode/difficult/FindMedianSortedArraysTest.java
@@ -0,0 +1,20 @@
+package com.huazie.fleaframework.algorithm.leetcode.difficult;
+
+import org.junit.Test;
+
+public class FindMedianSortedArraysTest {
+
+    private FindMedianSortedArrays solution = new FindMedianSortedArrays();
+
+    @Test
+    public void findMedianSortedArrays() {
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+
+        System.out.println(solution.findMedianSortedArrays(nums1, nums2));
+
+        int[] nums3 = {1, 2};
+        int[] nums4 = {3, 4};
+        System.out.println(solution.findMedianSortedArrays(nums3, nums4));
+    }
+}
