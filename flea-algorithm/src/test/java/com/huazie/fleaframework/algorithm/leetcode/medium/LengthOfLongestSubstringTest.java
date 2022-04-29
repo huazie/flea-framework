cbf90cee9c6b6a3ab1a74b1ab0c908f58939407f
Huazie <huazie.lgh@gmail.com>
2022-04-29 23:55:16 +0800

    新增leetcode的算法题【简单、中等、困难】

---
+++ flea-algorithm/src/test/java/com/huazie/fleaframework/algorithm/leetcode/medium/LengthOfLongestSubstringTest.java
@@ -0,0 +1,20 @@
+package com.huazie.fleaframework.algorithm.leetcode.medium;
+
+import org.junit.Test;
+
+public class LengthOfLongestSubstringTest {
+
+    private LengthOfLongestSubstring soultion = new LengthOfLongestSubstring();
+
+    @Test
+    public void lengthOfLongestSubstring() {
+        String str1 = "abcabcbb";
+        System.out.println(soultion.lengthOfLongestSubstring(str1));
+
+        String str2 = "bbbbb";
+        System.out.println(soultion.lengthOfLongestSubstring(str2));
+
+        String str3 = "pwwkew";
+        System.out.println(soultion.lengthOfLongestSubstring(str3));
+    }
+}
