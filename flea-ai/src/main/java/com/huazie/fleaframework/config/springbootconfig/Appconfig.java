6970c988ab150e2f7b651ea2386e578d96a89234
lc <lc1669878193>
2024-12-23 22:33:36 +0800

    自定义 webclient 线程池配置

--- flea-ai/src/main/java/com/huazie/fleaframework/config/springbootconfig/Appconfig.java
+++ flea-ai/src/main/java/com/huazie/fleaframework/config/springbootconfig/Appconfig.java
@@ -2,7 +2,11 @@ package com.huazie.fleaframework.config.springbootconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.LoopResources;
 
 @Configuration
 public class Appconfig {
@@ -10,5 +14,15 @@ public class Appconfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        // 自定义 Netty 线程池配置
+        HttpClient httpClient = HttpClient.create()
+                .runOn(LoopResources.create("webclient-pool", 10, 20, true));  // 设置线程池大小，最小4，最大10
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient));
     }
 }
